bfd201ad70eae943c368a26cfdde07d92e9b2abf
Ben <superben9@Bens-MacBook-Pro.local>
2016-04-10 19:57:48 -0500

    dded secret message

--- secretmessage.py
+++ secretmessage.py
@@ -13,3 +13,15 @@
 #
 
 # Your code here.
+import os
+def rename_files():
+    file_list = os.listdir("/Users/superben9/Desktop/udacitylearntocode/workshop3python/pythonlesson3/prank")
+    print(file_list)
+    os.chdir("/Users/superben9/Desktop/udacitylearntocode/workshop3python/pythonlesson3/prank")
+    print(file_list)
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+
+rename_files()
+
+
